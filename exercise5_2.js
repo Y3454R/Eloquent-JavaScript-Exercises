7c33629d02191318b640ffa523be35707767531e
Samin Yeasar Abir <63140759+Y3454R@users.noreply.github.com>
2021-07-15 17:11:42 +0600

    Update exercise5_2.js

--- exercise5_2.js
+++ exercise5_2.js
@@ -6,6 +6,7 @@ function myLoop(val, test, update, body) {
     }
 }
 
+myLoop(3, n => n > 0, n => n - 1, console.log);
 
 
 // author's solution
@@ -13,4 +14,4 @@ function loop(start, test, update, body) {
     for (let value = start; test(value); value = update(value)) {
       body(value);
     }
-  }
+}
